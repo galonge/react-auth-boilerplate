34100a7681c63900c6e0abf6395b64340dd95d2d
George Alonge <georgedelson@gmail.com>
2019-05-08 12:15:34 -0400

    setup login form and front end validations

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,11 +1,24 @@
 import React from "react"; 
+import LoginForm from "../forms/LoginForm";
 // import { Link } from 'react-router-dom';
 
-const LoginPage = () => (
-    <div>
-        <h1>Login</h1>
-    </div>
-);
+class LoginPage extends React.Component {
+
+    submit = data => {
+        console.log(data);
+    }
+
+    render() {
+        return (
+            <div>
+             <h1>Login</h1>
+                <LoginForm submit={this.submit} />
+            </div>
+        );
+    }
+
+}
+
 
 
 export default LoginPage;
